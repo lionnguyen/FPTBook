0735f1ad39b35fc9fb8d99e31958c6e14d9a85e4
Nguyen Hieu <nguyenminhhieu811@gmail.com>
2022-05-11 09:08:07 +0700

    delete mail

--- FPTBook/Program.cs
+++ FPTBook/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using FPTBook.Data;
 using FPTBook.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity.UI.Services;
-using FPTBook.Email;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FPTBookContext>(options =>
@@ -19,16 +18,7 @@ builder.Services.AddDefaultIdentity<FPTBookUser>(options => options.SignIn.Requi
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 var config = builder.Configuration;
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-builder.Services.Configure<EmailSenderOptions>(options =>
-{
-    options.Host = config["MailSettings:Host"];
-    options.Port = int.Parse(config["MailSettings:Port"]);
-    options.User = config["MailSettings:User"];
-    options.Pass = config["MailSettings:Pass"];
-    options.Name = config["MailSettings:Name"];
-    options.Sender = config["MailSettings:User"];
-});
+
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
